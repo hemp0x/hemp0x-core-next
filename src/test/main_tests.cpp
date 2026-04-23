3a1db655bc7b0f909ae939129eccc0c6c420ed59
BCR <services@beyondcr.com>
2026-04-23 11:41:05 -0400

    test: fix subsidy and wallet expectations

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -17,7 +17,7 @@ BOOST_FIXTURE_TEST_SUITE(main_tests, TestingSetup)
     static void TestBlockSubsidyHalvings(const Consensus::Params &consensusParams)
     {
         int maxHalvings = 64;
-        CAmount nInitialSubsidy = 5000 * COIN;
+        CAmount nInitialSubsidy = 10 * COIN;
 
         CAmount nPreviousSubsidy = nInitialSubsidy * 2; // for height == 0
         BOOST_CHECK_EQUAL(nPreviousSubsidy, nInitialSubsidy * 2);
@@ -58,11 +58,11 @@ BOOST_FIXTURE_TEST_SUITE(main_tests, TestingSetup)
         for (int nHeight = 0; nHeight < 14000000; nHeight += 1000)
         {
             CAmount nSubsidy = GetBlockSubsidy(nHeight, chainParams->GetConsensus());
-            BOOST_CHECK(nSubsidy <= 5000 * COIN);
+            BOOST_CHECK(nSubsidy <= 10 * COIN);
             nSum += nSubsidy * 1000;
             BOOST_CHECK(MoneyRange(nSum));
         }
-        BOOST_CHECK_EQUAL(nSum, (int64_t)2078125000000000000ULL);
+        BOOST_CHECK_EQUAL(nSum, (int64_t)14000000000000000ULL);
     }
 
     bool ReturnFalse()
